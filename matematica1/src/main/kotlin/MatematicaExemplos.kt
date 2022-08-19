33a4d73606388742125fdada6750023ac78373ef
Alexgoi <86365984+Alexgoi@users.noreply.github.com>
2022-08-18 21:54:01 -0300

    Add files via upload

---
+++ matematica1/src/main/kotlin/MatematicaExemplos.kt
@@ -0,0 +1,23 @@
+import kotlin.math.pow // formula para calcular potencia
+import kotlin.math.sqrt
+// kotlin.math para importar calculos matematicos
+
+
+fun main (){ // numeros para calculos com variaveis
+    val numero1 = 10.0
+    val numero2 = 2
+// calculos
+    val soma = numero1 + numero2
+    val subtracao = numero1 - numero2
+    val multip = numero1 * numero2
+    val divisao = numero1 / numero2
+
+
+    val potencia = numero1.pow(numero2)
+    val raiz = sqrt(numero1)
+
+
+
+    val resto = numero1 % 4 // resto da divisão (?)
+    print()
+}
