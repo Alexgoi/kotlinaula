3fbbf0c5e36d9a14c192c29b60f63df4d3aaff84
Alexgoi <86365984+Alexgoi@users.noreply.github.com>
2022-08-18 21:10:05 -0300

    Add files via upload

---
+++ aula2/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main (){ // variavel vai ser identifcada por var
+    var numero = 50 // atribuição dinamica de tipo, double para numeros quebrados, int para inteiros.
+    var outroNumero:Double = 85.0 // atribuição estatica de tipo
+
+    // exemplo diferente para receber um tipo de variavel
+    var idade:Int
+    idade = 10
+
+    //constante vai ser identificada por val
+    val nome = "Alex"
+
+    //exemplo para identificar a inicialização
+    val email:String
+
+    email = "alex@hotmail.com"
+
+}
